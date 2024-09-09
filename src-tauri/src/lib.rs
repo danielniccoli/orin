17d37bd71943abb89b02fd4fc5baf463c4d1a8c1
Daniel Niccoli <2971735+danielniccoli@users.noreply.github.com>
2024-09-09 21:53:23 +0200

    Add logging capabilities

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,10 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|_app| {
+            log::debug!("Logging is now enabled");
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
